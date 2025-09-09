f14fb9a9226818a7f7241056bc60562e728872d2
Hagermaher <hagermaherr337@gmail.com>
2025-09-09 03:55:20 +0300

    state component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
 
 
-function App() {
-  let task="study";
-  function handlesubmit(e) {
+
+class App extends React.Component{
+
+  constructor(){
+    //TO access this
+    super();
+    //TO DEFINE STATE IN CONSTRUCT
+  this.state={task: "study"};
+  //BIND FUNCTION SCOPE
+  this.handlesubmit=this.handlesubmit.bind(this);
+  }
+
+
+  handlesubmit(e) {
     
     e.preventDefault();
-    task=e.target[0].value;
+    this.state.task=e.target[0].value;
+
 
 }
-
+//to access method you should use this 
+render(){
   return (
-    <div className="todo">
-     <form onSubmit={handlesubmit}>
-      <input placeholder='type your task'></input>
+    <div className="todo"> 
+     <form onSubmit={this.handlesubmit}>
+      <input placeholder="type your task"></input>
       <button>ADD</button>
-      <p>{task}</p>
-     </form>
-        
-    </div>
+      <p>{this.state.task}</p>
+     </form> 
+    </div> 
   );
+  }
 }
 
 export default App;
